76db829e489bac2a7eb37fc83472348854aff094
Kevin Østerkilde <kevin@oesterkilde.dk>
2025-08-03 14:13:39 +0200

    chore: remove more unused boilerplate
    Also added type-check script and corrected a small type mistake

--- packages/api/src/transactions/entities/transaction.entity.ts
+++ packages/api/src/transactions/entities/transaction.entity.ts
@@ -2,6 +2,7 @@ export class Transaction {
   id: string;
   userId: string;
   createdAt: string; // Stringified date
+  updatedAt?: string; // Stringified date
   type: 'payout' | 'spent' | 'earned';
   amount: number;
 }
